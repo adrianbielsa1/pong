0834880087bc0afd3e59478ff2122254f4a201e2
Adrián Bielsa <63594921+adrianbielsa1@users.noreply.github.com>
2021-02-01 17:15:10 -0300

    Added functions `width` and `height`

--- front-end/source-code/aabb.js
+++ front-end/source-code/aabb.js
@@ -6,4 +6,12 @@ export class AABB {
         this.right = right;
         this.bottom = bottom;
     }
+
+    get width() {
+        return Math.abs(this.left - this.right);
+    }
+
+    get height() {
+        return Math.abs(this.top - this.bottom);
+    }
 }
